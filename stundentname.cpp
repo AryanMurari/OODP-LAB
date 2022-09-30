5f2623ac4d92ae9276824dfce0b9f70960b50fd0
Aryan Murari <92181578+AryanMurari@users.noreply.github.com>
2022-09-30 09:22:08 +0530

    Add files via upload

---
+++ stundentname.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class student{
+    String ss;
+    String name;
+  public student(String ss){
+      name = ss;
+  }
+  public student()
+  {
+      name = "unknown";
+  }
+}
+public class Raju{
+    public static void main(String[] args) {
+        student obj = new student();
+        obj.ss = "Vivek";
+        //Student ss1 = new Student();
+        System.out.println(obj.ss);
+        System.out.println(obj.name);
+
+    }
+}
+int main()
+{
+    student(String ss);
+    student();
+    Raju;
+
+    return 0;
+}
